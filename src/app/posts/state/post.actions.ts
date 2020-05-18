11b46e75c0ceed1736e16b009378bbd458161993
Ayesha Jownally <sheik.bibi.ayesha.siddiqa.jownally@elca.mu>
2020-05-18 17:16:25 +0400

    add crud actions

--- src/app/posts/state/post.actions.ts
+++ src/app/posts/state/post.actions.ts
@@ -1,4 +1,5 @@
 import { Action } from '@ngrx/store';
+import { Update } from '@ngrx/entity';
 
 import { Post } from '../post.model';
 
@@ -6,8 +7,25 @@ export enum PostActionTypes {
   LOAD_POSTS = '[POST] Load Posts',
   LOAD_POSTS_SUCCESS = '[POST] Load Posts Success',
   LOAD_POSTS_FAILURE = '[POST] Load Posts Failure',
+
+  LOAD_POST = '[POST] Load Post',
+  LOAD_POST_SUCCESS = '[POST] Load Post Success',
+  LOAD_POST_FAILURE = '[POST] Load Post Failure',
+
+  ADD_POST = '[POST] Add Post',
+  ADD_POST_SUCCESS = '[POST] Add Post Success',
+  ADD_POST_FAILURE = '[POST] Add Post Failure',
+
+  UPDATE_POST = '[POST] Update Post',
+  UPDATE_POST_SUCCESS = '[POST] Update Post Success',
+  UPDATE_POST_FAILURE = '[POST] Update Post Failure',
+
+  DELETE_POST = '[POST] Delete Post',
+  DELETE_POST_SUCCESS = '[POST] Delete Post Success',
+  DELETE_POST_FAILURE = '[POST] Delete Post Failure',
 }
 
+// -----------LOAD POSTS ACTIONS-----------------//
 export class LoadPosts implements Action {
   readonly type = PostActionTypes.LOAD_POSTS;
 }
@@ -15,12 +33,88 @@ export class LoadPosts implements Action {
 export class LoadPostsSuccess implements Action {
   readonly type = PostActionTypes.LOAD_POSTS_SUCCESS;
 
-  constructor(public payload: Post[]) {}
+  constructor(public payload: Post[]) { }
 }
 export class LoadPostsFailure implements Action {
   readonly type = PostActionTypes.LOAD_POSTS_FAILURE;
 
-  constructor(public payload: string) {}
+  constructor(public payload: string) { }
 }
 
-export type ActionPosts = LoadPosts | LoadPostsSuccess | LoadPostsFailure;
+// -----------LOAD POST ACTIONS-----------------//
+export class LoadPost implements Action {
+  readonly type = PostActionTypes.LOAD_POST;
+
+  constructor(public payload: number) { }
+}
+
+export class LoadPostSuccess implements Action {
+  readonly type = PostActionTypes.LOAD_POST_SUCCESS;
+
+  constructor(public payload: Post) { }
+}
+export class LoadPostFailure implements Action {
+  readonly type = PostActionTypes.LOAD_POST_FAILURE;
+
+  constructor(public payload: string) { }
+}
+
+// -----------ADD POST ACTIONS-----------------//
+export class AddPost implements Action {
+  readonly type = PostActionTypes.ADD_POST;
+
+  constructor(public payload: Post) { }
+}
+
+export class AddPostSuccess implements Action {
+  readonly type = PostActionTypes.ADD_POST_SUCCESS;
+
+  constructor(public payload: Post) { }
+}
+export class AddPostFailure implements Action {
+  readonly type = PostActionTypes.ADD_POST_FAILURE;
+
+  constructor(public payload: string) { }
+}
+
+// -----------UPDATE POST ACTIONS-----------------//
+export class UpdatePost implements Action {
+  readonly type = PostActionTypes.UPDATE_POST;
+
+  constructor(public payload: Post) { }
+}
+
+export class UpdatePostSuccess implements Action {
+  readonly type = PostActionTypes.UPDATE_POST_SUCCESS;
+
+  constructor(public payload: Update<Post>) { }
+}
+export class UpdatePostFailure implements Action {
+  readonly type = PostActionTypes.UPDATE_POST_FAILURE;
+
+  constructor(public payload: string) { }
+}
+
+// -----------DELETE POST ACTIONS-----------------//
+export class DeletePost implements Action {
+  readonly type = PostActionTypes.DELETE_POST;
+
+  constructor(public payload: number) { }
+}
+
+export class DeletePostSuccess implements Action {
+  readonly type = PostActionTypes.DELETE_POST_SUCCESS;
+
+  constructor(public payload: number) { }
+}
+export class DeletePostFailure implements Action {
+  readonly type = PostActionTypes.DELETE_POST_FAILURE;
+
+  constructor(public payload: string) { }
+}
+
+export type ActionPosts = LoadPosts | LoadPostsSuccess | LoadPostsFailure |
+  LoadPost | LoadPostSuccess | LoadPostFailure |
+  AddPost | AddPostSuccess | AddPostFailure |
+  UpdatePost | UpdatePostSuccess | UpdatePostFailure |
+  DeletePost | DeletePostSuccess | DeletePostFailure;
--- src/app/posts/state/post.reducer.ts
+++ src/app/posts/state/post.reducer.ts
@@ -3,24 +3,31 @@ import { Post } from '../post.model';
 import * as fromRoot from '../../state/app-state';
 
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { EntityState, EntityAdapter, createEntityAdapter } from '@ngrx/entity';
 
-export interface PostState {
-  posts: Post[];
+export interface PostState extends EntityState<Post> {
+  selectedPostId: number | null;
   loading: boolean;
   loaded: boolean;
   error: string;
 }
 
-export interface AppState extends fromRoot.AppState {
-  posts: PostState;
-}
+export const postAdapter: EntityAdapter<Post> = createEntityAdapter<Post>();
 
-export const initialState: PostState = {
-  posts: [],
+export const defaultPost: PostState = {
+  ids: [],
+  entities: {},
+  selectedPostId: null,
   loading: false,
   loaded: false,
   error: ''
 };
+
+export interface AppState extends fromRoot.AppState {
+  posts: PostState;
+}
+
+export const initialState = postAdapter.getInitialState(defaultPost);
 
 export function postReducer(state = initialState, action: postActions.ActionPosts): PostState {
   switch (action.type) {
@@ -31,17 +38,16 @@ export function postReducer(state = initialState, action: postActions.ActionPost
       }
 
     case postActions.PostActionTypes.LOAD_POSTS_SUCCESS:
-      return {
+      return postAdapter.addAll(action.payload, {
         ...state,
         loading: false,
-        loaded: true,
-        posts: action.payload
-      }
+        loaded: true
+      });
 
     case postActions.PostActionTypes.LOAD_POSTS_FAILURE:
       return {
         ...state,
-        posts: [],
+        entities: {},
         loading: false,
         loaded: false,
         error: action.payload
@@ -59,7 +65,7 @@ const getPostFeatureState = createFeatureSelector<PostState>(
 
 export const getPosts = createSelector(
   getPostFeatureState,
-  (state: PostState) => state.posts
+  postAdapter.getSelectors().selectAll
 );
 
 export const getPostsLoading = createSelector(
